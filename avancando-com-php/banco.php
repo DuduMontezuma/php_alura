e6a4d8af1f8f1e24e0c2bd06aaec70400b118fe1
DuduMontezuma <carlos15montezuma@gmail.com>
2023-11-15 14:05:47 -0300

    Terminei a "Introdução do PHP na Web"

--- avancando-com-php/banco.php
+++ avancando-com-php/banco.php
@@ -9,7 +9,7 @@ $contasCorrentes = [
     ],
     '123.456.489-11' => [
         'titular' => 'Maria',
-        'saldo' => '10000'
+        'saldo' => 10000
     ],
     '123.256.189-10' => [
         'titular' => 'Alberto',
@@ -27,9 +27,27 @@ unset ($contasCorrentes['123.256.189-10']);
 
 titularComLetrasMaiusculas($contasCorrentes['123.456.489-11']);
 
-foreach ($contasCorrentes as $cpf => $conta) {
-    ['titular' => $titular, 'saldo' => $saldo] = $conta;
-    exibeMensagem(
-        mensagem: "$cpf $titular $saldo"
-    );
-}
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Contas Correntes</h1>
+
+    <dl>
+        <?php foreach($contasCorrentes as $cpf => $conta) { ?>
+        <dt>
+            <h3><?= $conta['titular']; ?> - <?= $cpf ?></h3>
+        </dt>
+        <dd>
+            Saldo: <?= $conta['saldo'];?>
+        </dd>
+        <?php } ?>
+    </dl>
+</body>
+</html>
