300aebe00da7965dd5645bbf06d7d5e265dcceee
DuduMontezuma <carlos15montezuma@gmail.com>
2023-11-15 17:56:41 -0300

    Terminei "Fazendo verificações"

--- manipulando-arrays/notas.php
+++ manipulando-arrays/notas.php
@@ -5,7 +5,27 @@ $notas = [
     'João' => 8,
     'Maria' => 9,
     'Roberto' => 7,
-    'Dudu' => 6
+    'Dudu' => null
 ];
 
 krsort($notas);
+var_dump($notas);
+
+if(is_array($notas)){
+    echo 'Sim, é um array' . PHP_EOL;
+}
+
+var_dump(array_is_list($notas));
+
+echo 'Dudu fez a prova: '. PHP_EOL;
+var_dump(isset($notas['Dudu']));
+
+echo 'Alguém tirou 10? ' . PHP_EOL;
+var_dump(in_array(10, $notas, true));
+
+echo 'Quem tirou 10?' . PHP_EOL;
+var_dump(array_search(10, $notas, true));
+
+// array_key_exists = verifica se a chave existe
+// in_array = verifica se o valor existe
+// isset = verifica se a chave existe e não é nula
